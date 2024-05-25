b7f3afd4769f0e82373ee5540c91e1b318f00bd7
medinastr <pedromstracieri@gmail.com>
2024-05-25 19:46:05 -0300

    begin - I just create Product class with getters and setters

---
+++ BlueVelvetMusicStore/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module musicstore.bluevelvetmusicstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens musicstore.bluevelvetmusicstore to javafx.fxml;
+    exports musicstore.bluevelvetmusicstore;
+}
---
+++ BlueVelvetMusicStore/src/main/java/musicstore/bluevelvetmusicstore/Main.java
@@ -0,0 +1,15 @@
+package musicstore.bluevelvetmusicstore;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+        product.setProductName("Guitar");
+        product.setDescription("You can play many musical types");
+        product.setCategory("Instrument");
+        product.setBrand("Strinberg");
+        product.setCost(1199.90);
+        product.setListPrice(1000.00);
+    }
+}
